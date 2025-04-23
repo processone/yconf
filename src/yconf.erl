processone/yconf

commit 67c8fdf66581c7facf22f66970d7f2ce66387275

Add support to replace a macro used inside a string when surrounded with @

src/yconf.erl
@@ -934,7 +934,11 @@ replace_macro(V, {Name, Val}, Macros, Path) ->
 		false -> replace_macros(Val, Macros, [Name|Path])
 	    end;
 	_ ->
-	    V
+	    case is_binary(V) andalso is_binary(Val) of
+	        true -> re:replace(V, <<"@", Name/binary, "@">>, Val,
+                                   [{return, binary}, global]);
+	        false -> V
+	    end
     end;
 replace_macro(Val, _, _, _) ->
     Val.

commit d1b3c43b53d863150c1c9690d497f7cef57da62e

Support non-latin1 atoms, broken in 67c8fdf (processone/ejabberd#4369)

src/yconf.erl
@@ -975,7 +975,9 @@ read_include_files(Includes, Opts, Paths) ->
 %%%===================================================================
 -spec to_binary(term()) -> binary().
 to_binary(A) when is_atom(A) ->
-    atom_to_binary(A, latin1);
+    try erlang:atom_to_binary(A, latin1)
+    catch _:badarg -> erlang:atom_to_binary(A, utf8)
+    end;
 to_binary(B) when is_binary(B) ->
     B;
 to_binary(Bad) ->